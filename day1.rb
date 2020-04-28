ce4cfdf0c53c142fb1cba0e5f96b4144b7dc393e
a20a <aliakbar4ac@gmail.com>
2020-04-29 00:25:14 +0200

    added instruction file and updated the day1 rb code

---
+++ day1.rb
@@ -0,0 +1,21 @@
+=begin
+
+::29th April 2020::
+Day1: exploring general Ruby syntax and methodes  
+
+=end
+
+print "WHAT IS YOUR NAME?"
+first_name=gets.chomp
+first_name.capitalize!
+print "ENTER YOUR LAST NAME"
+last_name=gets.chomp
+last_name.capitalize!
+print "enter your city name"
+city=gets.chomp
+city.capitalize!
+print "enter your state"
+state=gets.chomp
+state=state.upcase!
+
+puts "#{first_name} is from #{city} which is in #{state}"
